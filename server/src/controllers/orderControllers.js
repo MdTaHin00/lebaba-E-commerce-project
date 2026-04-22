ad68736e771905faa97e1079179ddbfcc132d8b6
tahin <mdtahinhassanjihad@gmail.com>
2026-04-22 22:15:45 +0600

    payment delete,update,single payment show

--- server/src/controllers/orderControllers.js
+++ server/src/controllers/orderControllers.js
@@ -51,49 +51,149 @@ const makePaymentRequest = async (req, res) => {
 
 //! stripe website confirm payment system
 //? post method
-const confirmPayment = async(req,res) => { 
-  const {session_id} = req.body;
-  
-  try { 
-    //* stripe code confirm payment janno
-     const session = await stripe.checkout.sessions.retrieve(session_id,{
-         //* payment_intent -> stripe default cose 
-        expand:["line_items","payment_intent"]
-     })
-       //* stripe code confirm payment janno
-     const paymentIntentId = session.payment_intent.id;
-    
-     let order = await Order.findOne({orderId:paymentIntentId})
-  
-     if(!order){
-         //* stripe code confirm payment janno
-        const lineItems = session.line_items.data.map((item)=>({
-            productId: item.price.product,
-            quantity: item.quantity
-        }))
-        //* makePaymentRequest function a unit_amount jata add korce ota bat dece
-        const amount = session.amount_total / 100
-        order = new Order({
-             orderId:paymentIntentId,
-             products:lineItems,
-             amount:amount,
-             email:session.customer_details.email,
-             status: session.payment_intent.status ==="succeeded" ? "pending" : "failed"
+const confirmPayment = async (req, res) => {
+    const { session_id } = req.body;
+
+    try {
+        //* stripe code confirm payment janno
+        const session = await stripe.checkout.sessions.retrieve(session_id, {
+            //* payment_intent -> stripe default cose 
+            expand: ["line_items", "payment_intent"]
         })
-     }else{
-        order.status = session.payment_intent.status === "succeeded" ? "pending" : "failed"
-     }
+        //* stripe code confirm payment janno
+        const paymentIntentId = session.payment_intent.id;
 
-     await order.save()
+        let order = await Order.findOne({ orderId: paymentIntentId })
 
-    return successResponse(res,202,"Order Confirmed successfully",order)
-    
-} catch (error) {
-   return errorResponse(res,404,"Failed to confirm payment") 
-  }
+        if (!order) {
+            //* stripe code confirm payment janno
+            const lineItems = session.line_items.data.map((item) => ({
+                productId: item.price.product,
+                quantity: item.quantity
+            }))
+            //* makePaymentRequest function a unit_amount jata add korce ota bat dece
+            const amount = session.amount_total / 100
+            order = new Order({
+                orderId: paymentIntentId,
+                products: lineItems,
+                amount: amount,
+                email: session.customer_details.email,
+                status: session.payment_intent.status === "succeeded" ? "pending" : "failed"
+            })
+        } else {
+            order.status = session.payment_intent.status === "succeeded" ? "pending" : "failed"
+        }
+        await order.save()
+        return successResponse(res, 202, "Order Confirmed successfully", order)
+
+    } catch (error) {
+        return errorResponse(res, 404, "Failed to confirm payment")
+    }
 }
 
 
+//! single order show by email
+const getOrdersByEmail = async (req, res) => {
+    const email = req.params.email
+    try {
+        if (!email) {
+            return errorResponse(res, 404, "Email not found")
+        }
+
+        const orders = await Order.find({ email }).sort({ createdAt: -1 })
+
+        if (orders.length === 0 || !orders) {
+            return errorResponse(res, 404, "No orders found for this email")
+        }
+        successResponse(res, 202, "Orders fetched successFully", orders)
+
+    } catch (error) {
+        errorResponse(res, 404, "Failed to get orders", error)
+    }
+}
+
+
+//! get orders by id 
+const getOrdersById = async(req,res)=>{
+    const id = req.params.id
+  try {
+    const order = await Order.findById(id)
+    if(!order){
+        return errorResponse(res,404,"Order not found")
+    }
+    successResponse(res,202,"Order fetched successFully", order)
+    } catch (error) {
+        errorResponse(res,404,"failed to get by orders id")
+    }
+   
+}
+
+
+//! get all orders data 
+ const getAllOrders = async(req,res)=>{
+    try {
+
+        const orders = await Order.find().sort({createdAt:-1}) 
+
+        if(orders.length === 0 || !orders){
+            return errorResponse(res,404,"No Orders found")
+        }
+        return successResponse(res,202,"Orders Fetched successFully", orders)
+        
+    } catch (error) {
+    errorResponse(res,404,"Failed to get all orders",error)
+    }
+ }
+
+
+ //! update order status by id , patch method
+ const updateOrderStatus = async(req,res)=>{
+    const {id} = req.params;
+
+    const {status} = req.body
+    if(!status){
+        return errorResponse(res,404,"Status is required")
+    }
+
+    try {
+        const updateOrder = await Order.findByIdAndUpdate(id,{status:status, updatedAt:Date.now()} , {new:true,runValidators:true}) 
+
+        if(!updateOrder){
+            return errorResponse(res,404,"Order not found")
+        }
+
+        successResponse(res,202,"Order Status update successfully", updateOrder)
+
+    } catch (error) {
+        errorResponse(res,404,"Failed to update order status", error)
+    }
+ }
+
+
+ //! delete order by id 
+ //* delete method 
+ const deleteOrderById = async (req,res)=>{
+    const {id} = req.params 
+      try {
+        const deleteOrder = await Order.findByIdAndDelete(id)
+
+        if(!deleteOrder){
+            return errorResponse(res,404,"Order not found")
+        }
+
+        successResponse(res,202,"Order deleted successFully", deleteOrder)
+
+      } catch (error) {
+        errorResponse(res,404,"Failed Order deleted", error)
+      }
+ }
+
 module.exports = {
-    makePaymentRequest, confirmPayment
+    makePaymentRequest,
+    confirmPayment,
+    getOrdersByEmail,
+    getOrdersById,
+    getAllOrders,
+    updateOrderStatus,
+    deleteOrderById
 };
--- server/src/routes/orderRouter.js
+++ server/src/routes/orderRouter.js
@@ -1,16 +1,32 @@
 const express = require('express')
-const { makePaymentRequest, confirmPayment } = require('../controllers/orderControllers')
+const { makePaymentRequest, confirmPayment, getOrdersByEmail, getOrdersById, getAllOrders, updateOrderStatus, deleteOrderById } = require('../controllers/orderControllers')
 const router = express.Router()
 //! last work
 //* module.export = router
 
 
-
 //? create checkout session
-router.post('/create-checkout-session', makePaymentRequest )
+router.post('/create-checkout-session', makePaymentRequest)
 
 //? confirm payment  
-router.post('/confirm-payment' , confirmPayment)
+router.post('/confirm-payment', confirmPayment)
+
+//? get orders by email 
+router.get('/:email', getOrdersByEmail)
+
+//? get orders by id 
+router.get("/order/:id", getOrdersById)
+
+//?  get all order (only admin)
+router.get("/", getAllOrders)
+
+//? update order status by id (only admin)
+//* patch method 
+router.patch("/update-order-status/:id", updateOrderStatus)
+
+//? delete order by id (only admin)
+//* delete method
+router.delete("/delete-order/:id",deleteOrderById)
 
 
 module.exports = router
