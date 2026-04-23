c5eb2a7fc5441d24192d9ffb7d06e8031a4aafaf
tahin <mdtahinhassanjihad@gmail.com>
2026-04-23 18:30:21 +0600

    frontend route method

---
+++ client/src/dashBoard/DashBoardLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from "react-router";
+
+function DashBoardLayout() {
+  return (
+    <div>
+      DashBoardLayout
+      <Outlet/>
+    </div>
+  )
+}
+
+export default DashBoardLayout
--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -8,6 +8,7 @@ import Register from "../components/Register";
 import ShopPage from "../page/shop/ShopPage";
 import SingleProducts from "../page/shop/productDetails/SingleProducts";
 import PaymentSuccess from "../page/shop/payment/PaymentSuccess";
+import DashBoardLayout from "../dashBoard/DashBoardLayout";
 
 const router = createBrowserRouter([
     {
@@ -16,25 +17,26 @@ const router = createBrowserRouter([
         //! page na thkla ai page show hova 
         errorElement: <ErrorPage />,
         children: [
+            //* children use kolay <outlet> use korta hoy
             {
                 path: '/',
                 element: <Home />
             },
             {
                 path: '/shop',
-                element: <ShopPage/>
+                element: <ShopPage />
             },
             {
-                path:'/shop/:id',
-                element:<SingleProducts/>
+                path: '/shop/:id',
+                element: <SingleProducts />
             },
             {
                 path: '/categories/:categoryName',
                 element: <CategoryPage />
             },
             {
-                path:'/success',
-                element:<PaymentSuccess/>
+                path: '/success',
+                element: <PaymentSuccess />
             },
             {
                 path: "/about",
@@ -45,12 +47,76 @@ const router = createBrowserRouter([
     //! ai route navbar and footer pava na 
     //* ai route App.jsx file ar moda na
     {
-        path:'/login',
-        element:<Login/>
+        path: '/login',
+        element: <Login />
     },
     {
-        path:"/register",
-        element:<Register/>
+        path: "/register",
+        element: <Register />
+    },
+    //!  dashboard route
+    {
+         //? ata absolute path tai "/dashboard"
+        path: "/dashboard",
+        element: <DashBoardLayout/> ,
+        children: [
+        //* children use kolay <outlet> use korta hoy
+        
+        //? root file ata thakava 
+        //* ai route ar dashboard page and ata page thakva
+        {
+          path:"",
+          element:<div>user dashboard</div>
+        },  
+        {
+              //? ata relative path tai "orders"  , "/" ata nai  
+              //* search path -> dashboard/orders
+               //* ai route ar dashboard page and orders page thakva
+              path:"orders",
+             element:<div>orders</div>
+            },
+            {
+                 //? ata relative path tai "payments"  , "/" ata nai 
+                 //* search path -> dashboard/payments
+                  //* ai route ar dashboard page and payments page thakva
+                 path:"payments",
+                element:<div>payment</div>
+            },
+            {
+                path:"profile",
+                element:<div>profile</div>
+            },
+            {
+                path:"reviews",
+                element:<div>reviews</div>
+            },
+
+            //! admin route
+            {
+                path:"admin",
+                element:<div>admin dashboard</div>
+            },
+            {
+                path:"add-product",
+                element:<div>add product</div>
+            },
+            {
+                path:"manage-product",
+                element:<div>manage product</div>
+            },
+            {
+                path:"update-product/:id",
+                element:<div>update product</div>
+            },
+            {
+                path:"manage-orders",
+                element:<div>manage-orders</div>
+            },
+            {
+                path:"users",
+                element:<div>users</div>
+            },
+        ]
     }
 ]);
 
